b84adabdc32759adcda53e8513f0ac44a42edcf4
xzqt23 <angelo.agualada@gmail.com>
2022-11-25 11:20:58 +0800

    About us

---
+++ About Us/assets/js/about.js
@@ -0,0 +1,15 @@
+AOS.init(); // animate on scroll
+
+//counter
+new PureCounter({
+    selector: "#secondCounter",
+    decimals: 1, 
+    separator: true
+});
+
+new PureCounter({
+    selector: ".purecounter",
+    separator: true
+});
+
+
