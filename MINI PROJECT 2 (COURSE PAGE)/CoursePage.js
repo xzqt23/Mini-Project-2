286e1e66b7c5821bca202edc39dec6d751e690f3
IsanJoe <nessanjoeipulan@gmail.com>
2022-11-28 21:15:23 +0800

    Updated Javascript and CSS animation for our Course page

--- MINI PROJECT 2 (COURSE PAGE)/CoursePage.js
+++ MINI PROJECT 2 (COURSE PAGE)/CoursePage.js
@@ -24,3 +24,33 @@ function myBack() {
     document.getElementById("learnMoreContent_1").style.display = 'none';
     document.getElementById("courseFirst").style.display = 'block';
 }
+
+function myLearn2() {
+    document.getElementById("learnMoreContent_2").style.display = 'block';
+    document.getElementById("courseSecond").style.display = 'none';
+}
+
+function myBack2() {
+    document.getElementById("learnMoreContent_2").style.display = 'none';
+    document.getElementById("courseSecond").style.display = 'block';
+}
+
+function myLearn3() {
+    document.getElementById("learnMoreContent_3").style.display = 'block';
+    document.getElementById("courseThird").style.display = 'none';
+}
+
+function myBack3() {
+    document.getElementById("learnMoreContent_3").style.display = 'none';
+    document.getElementById("courseThird").style.display = 'block';
+}
+
+function myLearn4() {
+    document.getElementById("learnMoreContent_4").style.display = 'block';
+    document.getElementById("courseFourth").style.display = 'none';
+}
+
+function myBack4() {
+    document.getElementById("learnMoreContent_4").style.display = 'none';
+    document.getElementById("courseFourth").style.display = 'block';
+}
