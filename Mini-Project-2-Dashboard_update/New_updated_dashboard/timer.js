2f1f8e6efaeb6062ce7c9a0de916100e957c42e3
Masterkent11 <clementirkent1999@gmail.com>
2022-11-30 09:26:20 +0800

    this is the update dashboard with chat bot api

---
+++ Mini-Project-2-Dashboard_update/New_updated_dashboard/timer.js
@@ -0,0 +1,81 @@
+function showTime(){
+    var date = new Date();
+    var h = date.getHours(); // 0 - 23
+    var m = date.getMinutes(); // 0 - 59
+    var s = date.getSeconds(); // 0 - 59
+    var session = "AM";
+    
+    if(h == 0){
+        h = 12;
+    }
+    
+    if(h > 12){
+        h = h - 12;
+        session = "PM";
+    }
+    
+    h = (h < 10) ? "0" + h : h;
+    m = (m < 10) ? "0" + m : m;
+    s = (s < 10) ? "0" + s : s;
+    
+    var time = h + ":" + m + ":" + s + " " + session;
+    document.getElementById("MyClockDisplay").innerText = time;
+    document.getElementById("MyClockDisplay").textContent = time;
+    
+    setTimeout(showTime, 1000);
+    
+}
+
+showTime();
+
+
+
+// function myProgress(){
+
+//     if(displayProgress.style.display = 'none'){
+//         displayProgress.style.display = 'block'
+//     }
+//     else{
+//         displayProgress.style.display = 'none'
+//     }
+
+// }
+
+// function myComplete(){
+//     if(displayComplete.style.display = 'none'){
+//         displayComplete.style.display = 'block'
+//     }
+//     else{
+//         displayComplete.style.display = 'none'
+//     }
+
+// }
+
+var displayProgress = document.querySelector('#progressList')
+var displayComplete = document.querySelector('#completeList')
+
+
+function myProgress(){
+   if( displayProgress.style.display = 'none'){
+    displayProgress.style.display = 'block' 
+   }
+   else{
+    displayProgress.style.display = 'none'
+   }
+}
+
+
+function myComplete(){
+    if( displayComplete.style.display = 'none'){
+        displayComplete.style.display = 'block' 
+    }
+    else{
+        displayComplete.style.display = 'none'
+    }
+ }
+
+
+
+
+
+
